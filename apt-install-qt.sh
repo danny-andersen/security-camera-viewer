83fa4cfc5b572704a170bf80a149bb65ace61d8f
danny-andersen <danny_andersen@yahoo.com>
2025-11-12 08:52:34 +0000

    Added  Qt multimedia installs

--- apt-install-qt.sh
+++ apt-install-qt.sh
@@ -2,4 +2,6 @@ sudo apt update
 sudo apt install qtbase5-dev
 sudo apt install python3-pyqt5
 sudo apt install python3-pyqt5.qtwebengine
+sudo apt install python3-pyqt5.qtmultimedia
+sudo apt install libqt5multimedia5-plugins
 python -m venv --system-site-packages venv 
