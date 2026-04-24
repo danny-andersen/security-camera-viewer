32184132234d829f646558e7903352b86db898ae
danny-andersen <danny_andersen@yahoo.com>
2026-04-24 16:01:26 +0100

    Replaced Nano with ESP32 Nano to allow Wifi control so needed to change relay control due to 3V operation. Note no WIFI comms yet.

--- power_control/power_control.ino
+++ power_control/power_control.ino
@@ -1,17 +1,20 @@
+#include <Arduino.h>
+
 #define VSENSE_IN 3      // Connected to TV USB output - high when TV is on, low when off
 #define PICONTROL_OUT 4  // Tells the raspberry pi whether to shutdown or not. Must be high when the PI boots. Driving it low tells the PI to shutdown immediately
 #define RELAY_OUT 5      // Controls the power to the PI
 #define RED_LED 6
 #define GREEN_LED 7
 #define BLUE_LED 8
-#define RELAY_ON LOW  //Relay is low level triggered
-#define RELAY_OFF HIGH
+#define RELAY_ON HIGH  //Relay is low level triggered but is controlled via a transistor that needs to be driven high
+#define RELAY_OFF LOW
 #define TV_IS_ON LOW    //When TV is on, this line is driven low
 #define TV_IS_OFF HIGH  //When TV is off, this line is pulled high
 
 const unsigned long LOOP_DELAY = 1000;        // 1 sec loop time
 const unsigned long BOOT_DELAY = 300000;      // 5 mins boot up and settle time - minimum on period - will not enter a power off cycle until this has elapsed
 const unsigned long SHUTDOWN_DELAY = 120000;  // 2 mins shutdown time - will hold the power on for this period whilst the PI shutsdown and will not enter a new power on cycle
+const unsigned long TV_OFF_DEADBAND = 30000; // 30 seconds wait time after TV goes off before telling PI to power down
 unsigned long waitTime = 0;
 
 enum Colours {
@@ -33,7 +36,7 @@ enum State {
 };
 
 #define IDNAME(name) #name
-const char* stateNames[] = { IDNAME(INIT), IDNAME(TV_OFF), IDNAME(TV_ON), IDNAME(POWER_ON_PI), IDNAME(POWER_OFF_PI) };
+const char* stateNames[] = { IDNAME(INIT), IDNAME(TV_OFF), IDNAME(TV_ON), IDNAME(POWER_ON_PI), IDNAME(POWER_ON_PI_WAIT), IDNAME(POWER_OFF_PI), IDNAME(POWER_OFF_PI_WAIT) };
 
 int state = INIT;
 int newState = state;
@@ -42,22 +45,24 @@ void setup() {
   Serial.begin(38400);
   pinMode(VSENSE_IN, INPUT_PULLUP);
   pinMode(PICONTROL_OUT, OUTPUT);
-  pinMode(RED_LED, OUTPUT);    // Initialize the LED_BUILTIN pin as an output
-  pinMode(GREEN_LED, OUTPUT);  // Initialize the LED_BUILTIN pin as an output
-  pinMode(BLUE_LED, OUTPUT);   // Initialize the LED_BUILTIN pin as an output
-  pinMode(RELAY_OUT, OUTPUT);  // Initialize the LED_BUILTIN pin as an output
+  pinMode(RED_LED, OUTPUT);    // Initialize the LED pin as an output
+  pinMode(GREEN_LED, OUTPUT);  // Initialize the LED pin as an output
+  pinMode(BLUE_LED, OUTPUT);   // Initialize the LED pin as an output
+  pinMode(RELAY_OUT, OUTPUT);  // Initialize the RELAY pin as an output
 
   digitalWrite(PICONTROL_OUT, LOW);
   digitalWrite(RELAY_OUT, RELAY_OFF);
   setLED(RED);
+  state = INIT;
+  newState = INIT;
 }
 
 void loop() {
   int tvState = digitalRead(VSENSE_IN);
-  // Serial.print("State: ");
-  // Serial.print(stateNames[state]);
-  // Serial.print(" , TV is ");
-  // Serial.println(tvState);
+  Serial.print("State: ");
+  Serial.print(stateNames[state]);
+  Serial.print(" , TV is ");
+  Serial.println(tvState);
   unsigned long delayTime = LOOP_DELAY;
   switch (state) {
     case (INIT):
@@ -78,17 +83,27 @@ void loop() {
     case (POWER_ON_PI_WAIT):
       //Time deadband to allow PI time to boot up and load any security patches etc
       waitTime -= LOOP_DELAY;
-      if (waitTime <= 0) {
+      if (waitTime <= 0 && tvState == TV_IS_ON) {
         newState = TV_ON;
+        delayTime = 10;
+      }
+      if (waitTime <= 0 && tvState == TV_IS_OFF) {
+        newState = POWER_OFF_PI;
         delayTime = 10;
       }
       break;
     case (TV_ON):
       if (tvState == TV_IS_OFF) {
-        delay(30000);  //Wait for 30 seconds
+        waitTime = TV_OFF_DEADBAND;
+        do {
+          delay(LOOP_DELAY);
+          waitTime -= LOOP_DELAY;
+          tvState = digitalRead(VSENSE_IN);
+          Serial.print("State is TV_IS_OFF and waiting to see if TV comes back on: TV is ");
+          Serial.println(tvState);
+        } while (waitTime > 0 && tvState == TV_IS_OFF);
         // Re-read the sense input. If TV is still off then signal PI to shutdown
         //This provides a time deadband on the sense input to allow the USB cable to unplugged and replugged in
-        tvState = digitalRead(VSENSE_IN);
         if (tvState == TV_IS_OFF) {
           newState = POWER_OFF_PI;
         }
@@ -101,10 +116,10 @@ void loop() {
       newState = POWER_OFF_PI_WAIT;
       break;
     case (POWER_OFF_PI_WAIT):
-      //Time deadband to wait for PI to recognise shutdown has been signalled and to perform and controlled shutdown
+      //Time deadband to wait for PI to recognise shutdown has been signalled and to perform a controlled shutdown
       waitTime -= LOOP_DELAY;
       if (waitTime <= 0) {
-        newState = TV_OFF;
+        newState = TV_OFF; //Regardless of whether TV has come on or not, power off the PI as the PI Will have shutdown
         delayTime = 10;
       }
       break;
@@ -125,15 +140,15 @@ void loop() {
   }
   if (state == POWER_OFF_PI_WAIT && newState == TV_OFF) {
     digitalWrite(RELAY_OUT, RELAY_OFF);
-    delay(5000);  //Ensure PI is powercycled
     setLED(RED);
+    delay(5000);  //Ensure PI is powercycled
   }
   state = newState;
 }
 
 void setLED(Colours colour) {
-  // Serial.print("Setting colour to ");
-  // Serial.println(colour);
+  Serial.print("Setting colour to ");
+  Serial.println(colour);
   switch (colour) {
     case (OFF):
       digitalWrite(RED_LED, LOW);
